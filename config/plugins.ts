4660b4ce45e5dddddd0f32d77e31e856904c2a8a
Rob Rollenberg <rob@jepr.nl>
2025-06-16 20:06:36 +0200

    Added plugins for images and add github deploy script

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,32 @@
-export default () => ({});
+export default () => ({
+    upload: {
+        config: {
+            breakpoints: {
+                128: 128,
+                256: 256,
+                384: 384,
+                512: 512,
+                768: 768,
+                1024: 1024,
+                1280: 1280,
+                1536: 1536,
+                2048: 2048,
+            },
+            providerOptions: {
+                localServer: {
+                    maxage: 300000 * 12 * 24 * 365,
+                },
+            },
+        },
+    },
+    'webp-converter': {
+        enabled: true,
+        config: {
+            // mimeTypes that converts to WebP. Default is ['image/png', 'image/jpeg', 'image/jpg']
+            mimeTypes: undefined,
+            options: {
+                // WebP options: https://sharp.pixelplumbing.com/api-output#webp
+            },
+        },
+    },
+});
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,123 +1,112 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
+export interface BoxesBentoGridBox extends Struct.ComponentSchema {
+  collectionName: 'components_boxes_bento_grid_boxes';
   info: {
-    displayName: 'Seo';
-    icon: 'alien';
-    description: '';
+    displayName: 'Bento grid box';
+    icon: 'chartPie';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String;
-    metaDescription: Schema.Attribute.Text;
+    content: Schema.Attribute.RichText;
     image: Schema.Attribute.Media<'images'>;
-    keywords: Schema.Attribute.String;
-    preventIndexing: Schema.Attribute.Boolean &
-      Schema.Attribute.DefaultTo<false>;
+    position: Schema.Attribute.Enumeration<
+      ['left', 'centerTop', 'centerBottom', 'right']
+    >;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface SectionComponentTextWithImageBox
-  extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_text_with_image_boxes';
+export interface BoxesTextOnlyBox extends Struct.ComponentSchema {
+  collectionName: 'components_boxes_text_only_boxes';
   info: {
-    displayName: 'Text with image box';
+    displayName: 'Text only box';
+    icon: 'archive';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface CtaDefault extends Struct.ComponentSchema {
+  collectionName: 'components_cta_defaults';
+  info: {
+    displayName: 'Default';
+    icon: 'archive';
+  };
+  attributes: {
+    action: Schema.Attribute.Component<'links.button', false>;
+    call: Schema.Attribute.Text;
+  };
+}
+
+export interface LinksButton extends Struct.ComponentSchema {
+  collectionName: 'components_links_buttons';
+  info: {
+    displayName: 'Button';
+    icon: 'arrowRight';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+    variant: Schema.Attribute.Enumeration<['primary', 'secondary', 'tertiary']>;
+  };
+}
+
+export interface LinksTextLink extends Struct.ComponentSchema {
+  collectionName: 'components_links_text_links';
+  info: {
+    displayName: 'Text link';
+    icon: 'arrowRight';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface SectionComponentBentoGrid extends Struct.ComponentSchema {
+  collectionName: 'components_sections_component_bento_grids';
+  info: {
+    description: '';
+    displayName: 'Bento grid';
+    icon: 'apps';
+  };
+  attributes: {
+    bentoGridBox: Schema.Attribute.Component<'boxes.bento-grid-box', true> &
+      Schema.Attribute.SetMinMax<
+        {
+          max: 4;
+          min: 4;
+        },
+        number
+      >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionComponentBlogContentOnly
+  extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_blog_content_onlies';
+  info: {
+    displayName: 'Blog content only';
     icon: 'chartBubble';
-    description: '';
   };
   attributes: {
-    title: Schema.Attribute.String & Schema.Attribute.Required;
+    content: Schema.Attribute.RichText;
+  };
+}
+
+export interface SectionComponentBlogImageOnly extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_blog_image_onlies';
+  info: {
+    description: '';
+    displayName: 'Blog image only';
+    icon: 'brush';
+  };
+  attributes: {
     image: Schema.Attribute.Media<'images'>;
-    content: Schema.Attribute.RichText;
-    imageDirection: Schema.Attribute.Enumeration<['left', 'right']> &
-      Schema.Attribute.DefaultTo<'right'>;
-    cta: Schema.Attribute.Component<'cta.default', false>;
-  };
-}
-
-export interface SectionComponentHeroTextWithImage
-  extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_hero_text_with_images';
-  info: {
-    displayName: 'Hero text with image';
-    icon: 'briefcase';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    content: Schema.Attribute.RichText;
-    image: Schema.Attribute.Media<'images' | 'files'>;
-    primaryButton: Schema.Attribute.Component<'links.button', false>;
-    secondaryButton: Schema.Attribute.Component<'links.button', false>;
-    titleImage: Schema.Attribute.Media<'images'>;
-  };
-}
-
-export interface SectionComponentHeroTextOnly extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_hero_text_onlies';
-  info: {
-    displayName: 'Hero text only';
-    icon: 'apps';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    content: Schema.Attribute.Text;
-  };
-}
-
-export interface SectionComponentFullSizeText extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_full_size_texts';
-  info: {
-    displayName: 'Full size text';
-    icon: 'bold';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String & Schema.Attribute.Required;
-    content: Schema.Attribute.RichText;
-  };
-}
-
-export interface SectionComponentFaq extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_faqs';
-  info: {
-    displayName: 'Faq';
-    icon: 'italic';
-    description: '';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    textOnlyBox: Schema.Attribute.Component<'boxes.text-only-box', true> &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 3;
-          max: 12;
-        },
-        number
-      >;
-    content: Schema.Attribute.RichText;
-  };
-}
-
-export interface SectionComponentBoxGrid extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_box_grids';
-  info: {
-    displayName: 'Box grid';
-    icon: 'apps';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    content: Schema.Attribute.RichText;
-    backgroundImage: Schema.Attribute.Media<'images'>;
-    textOnlyBox: Schema.Attribute.Component<'boxes.text-only-box', true> &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 3;
-          max: 12;
-        },
-        number
-      >;
+    subTextForImage: Schema.Attribute.Text;
   };
 }
 
@@ -135,44 +124,20 @@ export interface SectionComponentBlogImageWithText
   };
 }
 
-export interface SectionComponentBlogImageOnly extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_blog_image_onlies';
+export interface SectionComponentBoxGrid extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_box_grids';
   info: {
-    displayName: 'Blog image only';
-    icon: 'brush';
-    description: '';
-  };
-  attributes: {
-    image: Schema.Attribute.Media<'images'>;
-    subTextForImage: Schema.Attribute.Text;
-  };
-}
-
-export interface SectionComponentBlogContentOnly
-  extends Struct.ComponentSchema {
-  collectionName: 'components_section_component_blog_content_onlies';
-  info: {
-    displayName: 'Blog content only';
-    icon: 'chartBubble';
-  };
-  attributes: {
-    content: Schema.Attribute.RichText;
-  };
-}
-
-export interface SectionComponentBentoGrid extends Struct.ComponentSchema {
-  collectionName: 'components_sections_component_bento_grids';
-  info: {
-    displayName: 'Bento grid';
+    displayName: 'Box grid';
     icon: 'apps';
-    description: '';
   };
   attributes: {
-    bentoGridBox: Schema.Attribute.Component<'boxes.bento-grid-box', true> &
+    backgroundImage: Schema.Attribute.Media<'images'>;
+    content: Schema.Attribute.RichText;
+    textOnlyBox: Schema.Attribute.Component<'boxes.text-only-box', true> &
       Schema.Attribute.SetMinMax<
         {
-          min: 4;
-          max: 4;
+          max: 12;
+          min: 3;
         },
         number
       >;
@@ -180,90 +145,125 @@ export interface SectionComponentBentoGrid extends Struct.ComponentSchema {
   };
 }
 
-export interface LinksTextLink extends Struct.ComponentSchema {
-  collectionName: 'components_links_text_links';
+export interface SectionComponentFaq extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_faqs';
   info: {
-    displayName: 'Text link';
-    icon: 'arrowRight';
+    description: '';
+    displayName: 'Faq';
+    icon: 'italic';
   };
   attributes: {
-    title: Schema.Attribute.String;
-    url: Schema.Attribute.String;
-  };
-}
-
-export interface LinksButton extends Struct.ComponentSchema {
-  collectionName: 'components_links_buttons';
-  info: {
-    displayName: 'Button';
-    icon: 'arrowRight';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
-    url: Schema.Attribute.String;
-    variant: Schema.Attribute.Enumeration<['primary', 'secondary', 'tertiary']>;
-  };
-}
-
-export interface CtaDefault extends Struct.ComponentSchema {
-  collectionName: 'components_cta_defaults';
-  info: {
-    displayName: 'Default';
-    icon: 'archive';
-  };
-  attributes: {
-    call: Schema.Attribute.Text;
-    action: Schema.Attribute.Component<'links.button', false>;
-  };
-}
-
-export interface BoxesTextOnlyBox extends Struct.ComponentSchema {
-  collectionName: 'components_boxes_text_only_boxes';
-  info: {
-    displayName: 'Text only box';
-    icon: 'archive';
-  };
-  attributes: {
-    title: Schema.Attribute.String;
     content: Schema.Attribute.RichText;
+    textOnlyBox: Schema.Attribute.Component<'boxes.text-only-box', true> &
+      Schema.Attribute.SetMinMax<
+        {
+          max: 12;
+          min: 3;
+        },
+        number
+      >;
+    title: Schema.Attribute.String;
   };
 }
 
-export interface BoxesBentoGridBox extends Struct.ComponentSchema {
-  collectionName: 'components_boxes_bento_grid_boxes';
+export interface SectionComponentFullSizeText extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_full_size_texts';
   info: {
-    displayName: 'Bento grid box';
-    icon: 'chartPie';
+    description: '';
+    displayName: 'Full size text';
+    icon: 'bold';
   };
   attributes: {
-    title: Schema.Attribute.String;
     content: Schema.Attribute.RichText;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SectionComponentHeroTextOnly extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_hero_text_onlies';
+  info: {
+    description: '';
+    displayName: 'Hero text only';
+    icon: 'apps';
+  };
+  attributes: {
+    content: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SectionComponentHeroTextWithImage
+  extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_hero_text_with_images';
+  info: {
+    description: '';
+    displayName: 'Hero text with image';
+    icon: 'briefcase';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    image: Schema.Attribute.Media<'images' | 'files'>;
+    primaryButton: Schema.Attribute.Component<'links.button', false>;
+    secondaryButton: Schema.Attribute.Component<'links.button', false>;
+    title: Schema.Attribute.String;
+    titleImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SectionComponentTextWithImageBox
+  extends Struct.ComponentSchema {
+  collectionName: 'components_section_component_text_with_image_boxes';
+  info: {
+    description: '';
+    displayName: 'Text with image box';
+    icon: 'chartBubble';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    cta: Schema.Attribute.Component<'cta.default', false>;
     image: Schema.Attribute.Media<'images'>;
-    position: Schema.Attribute.Enumeration<
-      ['left', 'centerTop', 'centerBottom', 'right']
-    >;
+    imageDirection: Schema.Attribute.Enumeration<['left', 'right']> &
+      Schema.Attribute.DefaultTo<'right'>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'alien';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+    keywords: Schema.Attribute.String;
+    metaDescription: Schema.Attribute.Text;
+    metaTitle: Schema.Attribute.String;
+    preventIndexing: Schema.Attribute.Boolean &
+      Schema.Attribute.DefaultTo<false>;
   };
 }
 
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.seo': SharedSeo;
-      'section-component.text-with-image-box': SectionComponentTextWithImageBox;
-      'section-component.hero-text-with-image': SectionComponentHeroTextWithImage;
-      'section-component.hero-text-only': SectionComponentHeroTextOnly;
-      'section-component.full-size-text': SectionComponentFullSizeText;
-      'section-component.faq': SectionComponentFaq;
-      'section-component.box-grid': SectionComponentBoxGrid;
-      'section-component.blog-image-with-text': SectionComponentBlogImageWithText;
-      'section-component.blog-image-only': SectionComponentBlogImageOnly;
-      'section-component.blog-content-only': SectionComponentBlogContentOnly;
-      'section-component.bento-grid': SectionComponentBentoGrid;
-      'links.text-link': LinksTextLink;
-      'links.button': LinksButton;
-      'cta.default': CtaDefault;
-      'boxes.text-only-box': BoxesTextOnlyBox;
       'boxes.bento-grid-box': BoxesBentoGridBox;
+      'boxes.text-only-box': BoxesTextOnlyBox;
+      'cta.default': CtaDefault;
+      'links.button': LinksButton;
+      'links.text-link': LinksTextLink;
+      'section-component.bento-grid': SectionComponentBentoGrid;
+      'section-component.blog-content-only': SectionComponentBlogContentOnly;
+      'section-component.blog-image-only': SectionComponentBlogImageOnly;
+      'section-component.blog-image-with-text': SectionComponentBlogImageWithText;
+      'section-component.box-grid': SectionComponentBoxGrid;
+      'section-component.faq': SectionComponentFaq;
+      'section-component.full-size-text': SectionComponentFullSizeText;
+      'section-component.hero-text-only': SectionComponentHeroTextOnly;
+      'section-component.hero-text-with-image': SectionComponentHeroTextWithImage;
+      'section-component.text-with-image-box': SectionComponentTextWithImageBox;
+      'shared.seo': SharedSeo;
     }
   }
 }
